96c9f91215d995da0bea392bd5992469cfc0e17d
Junlu Cheng <frank4565@gmail.com>
2013-05-25 11:14:20 +0800

    Added UI elements as properties.

--- HTTPCake/HCAppDelegate.h
+++ HTTPCake/HCAppDelegate.h
@@ -11,5 +11,12 @@
 @interface HCAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *urlTextField;
+@property (weak) IBOutlet NSButton *getButton;
+@property (weak) IBOutlet NSButton *postButton;
+@property (weak) IBOutlet NSTableView *requestHeadersTableView;
+@property (weak) IBOutlet NSTableView *parametersTableView;
+@property (unsafe_unretained) IBOutlet NSTextView *responseTextView;
+@property (unsafe_unretained) IBOutlet NSTextView *responseHeadersTextView;
 
 @end
